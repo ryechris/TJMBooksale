7f6b55ac470f510ae361a9ba95943f6bf33be4d4
call900913 <call900913@icloud.com>
2020-07-02 17:21:36 +0700

    Update to book cover only

--- src/Book.js
+++ src/Book.js
@@ -31,15 +31,6 @@ class Book extends Component {
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: thumbnail }}></div>
-            <div className="book-shelf-changer">
-              <select value={ shelf || "none" } onChange={(event) => this.props.handleChange(id, event.target.value)}>
-                <option value="move" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
           </div>
           <div className="book-title">{title}</div>
           <div className="book-authors">{author}</div>
