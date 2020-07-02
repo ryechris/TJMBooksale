96b443a8153b8cf7ec7d876094ef3c973124bb19
call900913 <call900913@icloud.com>
2020-07-02 17:56:52 +0700

    Fix bug

--- src/Book.js
+++ src/Book.js
@@ -31,6 +31,8 @@ class Book extends Component {
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: thumbnail }}></div>
+            <div className="book-shelf-changer">
+            </div>
           </div>
           <div className="book-title">{title}</div>
           <div className="book-authors">{author}</div>
